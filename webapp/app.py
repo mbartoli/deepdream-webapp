d17e49fd41ef1b4b7873ee41106856dd881e90b6
Mike Bartoli <mbartoli@users.noreply.github.com>
2015-07-09 11:42:09 -0400

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -1,15 +1,10 @@
-import os
-
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    provider = str(os.environ.get('PROVIDER', 'world'))
-    return 'Hello '+provider+'!'
+def hello_world():
+    return 'Hello, Flask World!\n'
 
 if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run('127.0.0.1', debug=True)
