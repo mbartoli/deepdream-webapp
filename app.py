8e0c30bf8f28c320a9d46ab8262ea30ae19ef180
mbartoli <bartolimichael@gmail.com>
2015-07-13 09:49:36 -0400

    adding call to deepdream to flask

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask
-
+import dream.py as dream
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    img = np.float32(PIL.Image.open('sky1024px.jpg'))
+    _ = dream.deepdream(net, img)
+
+
+# The complexity of the details generated depends on which layer's activations we try to maximize. Higher layers produce complex features, while lower ones enhance edges and textures, giving the image an impressionist feeling:
+
+# In[ ]:
+
+    _ = dream.deepdream(net, img, end='inception_3b/5x5_reduce')
     return "Deepdream inside Docker!!"
 
 
