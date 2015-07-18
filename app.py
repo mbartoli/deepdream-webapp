bbf84106fae8a64269365b3ac55d80f6572d36f5
Mike Bartoli <mbartoli@users.noreply.github.com>
2015-07-18 08:56:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,9 @@ app = Flask(__name__)
 def hello():
     img = np.float32(PIL.Image.open('sky1024px.jpg'))
     _ = dream.deepdream(net, img)
-
-
-# The complexity of the details generated depends on which layer's activations we try to maximize. Higher layers produce complex features, while lower ones enhance edges and textures, giving the image an impressionist feeling:
-
-# In[ ]:
-
     _ = dream.deepdream(net, img, end='inception_3b/5x5_reduce')
     return "Deepdream inside Docker!!"
 
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
-
